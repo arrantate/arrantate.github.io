4df228ccaaa8c2bebf7bceaf74768a711ff86112
arrantate <arranmt@gmail.com>
2021-02-15 16:13:28 +0000

    Remove: console.log() in script

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 fetch('https://api.github.com/users/arrantate')
     .then(response => response.json())
     .then(data => {
-        console.log(data)
         document.querySelector("#githubUsername").innerHTML = data.login
         document.querySelector("#githubPublic").innerHTML = data.public_repos
         document.querySelector("#githubFollowers").innerHTML = data.followers
