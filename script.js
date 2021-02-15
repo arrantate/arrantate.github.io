c5a1de5c1809f03027ef158796d2603d53c2f1d8
arrantate <arranmt@gmail.com>
2021-02-15 15:25:37 +0000

    Add: github section

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const year = document.querySelector("#year")
+fetch('https://api.github.com/users/arrantate')
+    .then(response => response.json()) //Converting the response to a JSON object
+    .then(data => {
+        console.log(data)
+        document.querySelector("#githubUsername").innerHTML = data.login
+        document.querySelector("#githubPublic").innerHTML = data.public_repos
+        document.querySelector("#githubFollowers").innerHTML = data.followers
+    })
+    .catch(error => console.error(error));
 
-document.addEventListener("DOMContentLoaded", () => {
-    year.innerHTML = new Date().getFullYear()
-})
+
+
