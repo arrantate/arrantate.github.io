c0adf56de05662a6fe941df465867fe585afd277
arrantate <arranmt@gmail.com>
2021-02-16 16:23:10 +0000

    Add: blinking underscore and drop down sections with JS

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Display user stats from the github API
 fetch('https://api.github.com/users/arrantate')
     .then(response => response.json())
     .then(data => {
@@ -6,3 +7,39 @@ fetch('https://api.github.com/users/arrantate')
         document.querySelector("#githubFollowers").innerHTML = data.followers
     })
     .catch(error => console.error(error));
+
+
+// Blinking underscore at the end of the github section
+const blinking_ = document.querySelector("#blinking_")
+const blinkInterval = 500
+const blink = () => {
+    if (blinking_.innerHTML === '_') {
+        blinking_.innerHTML = ''
+    } else {
+        blinking_.innerHTML = '_'
+    }
+}
+setInterval(blink, blinkInterval)
+
+
+// Make sections drop down on arrow click
+const arrows = document.querySelectorAll('.arrow')
+arrows.forEach(arrow => {
+    let arrowActive = true
+    arrow.onclick = () => {
+        console.log(arrow.dataset.class)
+        if (arrowActive) {
+            document.querySelectorAll(`.${arrow.dataset.class}`).forEach((div) => {
+                div.style.display = "none"
+                arrow.classList.add("rotate-90")
+            })
+            arrowActive = false
+        } else {
+            document.querySelectorAll(`.${arrow.dataset.class}`).forEach((div) => {
+                div.style.display = "block"
+                arrow.classList.remove("rotate-90")
+            })
+            arrowActive = true
+        }
+    }
+})
