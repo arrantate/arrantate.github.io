e346426209f3eca505f357c7985ca7410b4f2e88
arrantate <arranmt@gmail.com>
2021-02-18 17:36:44 +0000

    Add; local storage save

--- todo/todo.js
+++ todo/todo.js
@@ -3,16 +3,45 @@ const form = document.querySelector("#new-todo")
 const todoList = document.querySelector("#todo-list")
 const todoText = document.querySelector("#todo-text")
 
+
+// Listeners
+window.addEventListener("DOMContentLoaded", loadTodos)
 form.addEventListener("submit", addTodo)
+
+
+// render all todo items from localStorage
+function loadTodos() {
+    if (localStorage.getItem("todos") !== null) {
+        const todos = JSON.parse(localStorage.getItem("todos"))
+        todos.forEach(todo => {
+            const todoLi = document.createElement("Li")
+            todoLi.classList = "list-group-item text-left"
+            todoLi.innerHTML = todo
+            todoList.appendChild(todoLi)
+        })
+    } else {
+        localStorage.setItem("todos", JSON.stringify([]))
+    }
+}
+
+// Add new todo
 function addTodo(event) {
     event.preventDefault()
 
     const todoLi = document.createElement("Li")
     todoLi.classList = "list-group-item text-left"
     todoLi.innerHTML = todoText.value
-
     todoList.appendChild(todoLi)
-    console.log(localStorage.getItem("todos"))
+
+    saveTodo(todoText.value)
 
     todoText.value = ''
+}
+
+// Save input to local storage todos
+function saveTodo(todo) {
+    let todos = JSON.parse(localStorage.getItem("todos"))
+    todos.push(todo)
+    console.log(todos)
+    localStorage.setItem("todos", JSON.stringify(todos))
 }
