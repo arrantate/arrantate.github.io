c0b227cccffb067f2b73b86caea69d600cc1394e
arrantate <arranmt@gmail.com>
2021-02-24 19:32:32 +0000

    add: delete button

--- todo/todo.js
+++ todo/todo.js
@@ -9,15 +9,33 @@ window.addEventListener("DOMContentLoaded", loadTodos)
 form.addEventListener("submit", addTodo)
 
 
+// Create todo List Element
+function newListItem(todo) {
+    const todoLi = document.createElement("Li")
+    todoLi.classList = "d-flex"
+    todoList.appendChild(todoLi)
+
+    const todoText = document.createElement('p')
+    todoText.classList = "list-group-item text-left w-100 m-1"
+    todoText.innerText = todo
+    todoLi.appendChild(todoText)
+
+    const deleteBtn = document.createElement("Button")
+    deleteBtn.classList = "btn btn-danger w-20 ml-auto h-100 my-auto"
+    deleteBtn.onclick = deleteTodo
+    todoLi.appendChild(deleteBtn)
+
+    const btnIcon = document.createElement("i")
+    btnIcon.classList = "fas fa-trash"
+    deleteBtn.appendChild(btnIcon)
+}
+
 // render all todo items from localStorage
 function loadTodos() {
     if (localStorage.getItem("todos") !== null) {
         const todos = JSON.parse(localStorage.getItem("todos"))
         todos.forEach(todo => {
-            const todoLi = document.createElement("Li")
-            todoLi.classList = "list-group-item text-left"
-            todoLi.innerHTML = todo
-            todoList.appendChild(todoLi)
+            newListItem(todo)
         })
     } else {
         localStorage.setItem("todos", JSON.stringify([]))
@@ -26,13 +44,20 @@ function loadTodos() {
 
 // Add new todo
 function addTodo(event) {
+    if (todoText.value.length === 0) return null
     event.preventDefault()
-    const todoLi = document.createElement("Li")
-    todoLi.classList = "list-group-item text-left"
-    todoLi.innerHTML = todoText.value
-    todoList.appendChild(todoLi)
+    newListItem(todoText.value)
     saveTodo(todoText.value)
     todoText.value = ''
+}
+
+// Delete Todo
+function deleteTodo() {
+    const todoListItem = this.parentElement
+    const todoText = todoListItem.querySelector('p').innerText
+    todoListItem.remove()
+    console.log(`delete ${todoText}`)
+    removeTodo(todoText)
 }
 
 // Save input to local storage todos
@@ -41,4 +66,13 @@ function saveTodo(todo) {
     todos.push(todo)
     console.log(todos)
     localStorage.setItem("todos", JSON.stringify(todos))
+}
+
+// Remove input from local storage todos
+function removeTodo(toRemove) {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    console.log(todos)
+    const newTodos = todos.filter(todo => todo != toRemove)
+    console.log(newTodos)
+    localStorage.setItem("todos", JSON.stringify(newTodos))
 }
